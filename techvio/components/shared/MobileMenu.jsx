0bfa7190d4d8c89bc9cf2d6aa215e6628bc54501
Samira <samira.rahimi1998@gmail.com>
2023-02-12 15:13:29 -0500

    update

--- techvio/components/shared/MobileMenu.jsx
+++ techvio/components/shared/MobileMenu.jsx
@@ -1,8 +1,14 @@
-const MobileMenu = () => {
+const MobileMenu = ({ open, name }) => {
     const ItemStyle = 'p-5 border-b-5 text-lg font-bold'
+    const style = {
+        transform: `${open ? 'rotateX(0deg)' : 'rotateX(90deg)'}`
+    }
     return (
-        <div className="lg:hidden w-4/5 h-1/2 bg-white absolute top-20 left-0 right-0 mx-auto">
-            <div className={ItemStyle}>Home</div>
+        <div
+            className="lg:hidden w-4/5 h-1/2 bg-white absolute top-20 left-0 right-0 mx-auto origin-top transition-all duration-1000"
+            style={style}
+        >
+            <div className={ItemStyle}>{name}</div>
             <div className={ItemStyle}>About</div>
 
         </div>
@@ -11,3 +17,4 @@ const MobileMenu = () => {
 }
 
 export default MobileMenu
+   
